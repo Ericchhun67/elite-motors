7b3c896c9ccae40649207a3a251e62a3faa7093c
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-09-02 11:58:32 -0700

    Add app py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 """
 This is the main application file for the Elite Motors web application.
+date: 2026-07/17
 """
 
 from flask import Flask, render_template, request, redirect, url_for
 from config import Config, developmentConfig
 from extensions import db
 
-app = Flask(__name__) # Initialize the Flask application
+
+""" Flask application creates the app object and then stores it in the variable app.
+    the __name__ variable is a special python variable that is set to the name of
+    the module in which it is used. when the it is run directly
+"""
+app = Flask(__name__)
+
 app.config.from_object(Config)
 
 app.config.from_object(developmentConfig)
