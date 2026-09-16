52cb04f7536049070d4b389ea9f1223430e13507
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-09-15 19:47:51 -0700

    Add extensions py

--- extensions.py
+++ extensions.py
@@ -1,5 +1,10 @@
-""" for the sqlalchemy model """ 
+"""
+extensions.py
+
+This module initializes and configures the extensions using flask extensions like SQLAlchemy.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 
-
+# get the database  from SQLAlchemy instance
 db = SQLAlchemy()
